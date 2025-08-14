7a140c9614a514772006ce0f06f6e9c0c87c49ff
rsureka <rsureka@paypal.com>
2025-08-14 16:31:48 +0530

    no message

--- Arrays and Algorithms/Arrays.swift
+++ Arrays and Algorithms/Arrays.swift
@@ -153,7 +153,9 @@ class Arrays {
     var length = arr.count
     var lhs = 0
     var rhs = length - 1
-    
+    if length == 1 {
+      return arr[0]
+    }
     while (lhs <= rhs) {
       var mid = (rhs - lhs)/2 + lhs
       
@@ -192,4 +194,185 @@ class Arrays {
     }
     return -1
   }
+  
+  /*
+   Find peak element in an array.
+   Approach: The left side of peak element will have elements in increasing order and right side will have elements in dec order.
+   we can use this to eleminate the search space.
+   */
+  func findPeakElement(_ arr: [Int]) -> Int {
+    var length = arr.count
+    
+    if length == 1 {
+      return arr[0] //Only 1 element
+    }
+    var lhs = 0
+    var rhs = length - 1
+    
+    while lhs <= rhs {
+      var mid = (rhs - lhs)/2 + lhs
+      
+      if mid == 0 {
+        if arr[mid] > arr[mid + 1] {
+          return arr[mid]
+        } else {
+          lhs = mid + 1
+        }
+      } else if mid == length - 1 {
+        if arr[mid] > arr[mid - 1] {
+          return arr[mid]
+        } else {
+          rhs = mid - 1
+        }
+      } else {
+        if arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1] {
+          return arr[mid]
+        } else if arr[mid] > arr[mid - 1] {
+          lhs = mid + 1
+        } else {
+          rhs = mid - 1
+        }
+      }
+    }
+    return -1 //No peak found
+  }
+  
+  /*
+   Leetcode 875: Koko Eating Bananas
+   Find the minimum speed at which koko can eat all the bananas in a given hours.
+   Approach:
+   lets say an array as [2,3,5,6,1,9,4]
+   And hours as 8: so, max speed can be 9 and it will take 7 hours to eat all the bananas
+   min speed can be total as 30/8 = 4. At speed of 4 this take 11 hours to eat all.
+   1. The max speed is the maximum number of bananas in a pile. This will take time as length of array.
+   2. The min speed should be 1
+   3. The answer lies between min and max speed.
+   */
+  func koKoEatingBananas(_ bananas: [Int], _ hours: Int) -> Int {
+    var length = bananas.count
+    if hours < length {
+      return -1
+    }
+    
+    var maxSpeed = bananas.max()!
+    var minSpeed = 1
+    var answer = maxSpeed
+    
+    func calculateTime(_ speed: Int) -> Int {
+      var time = 0
+      for banana in bananas {
+        time += banana % speed == 0 ? (banana / speed) : (banana / speed) + 1
+      }
+      return time
+    }
+    
+    while minSpeed <= maxSpeed {
+      var midSpeed = (maxSpeed - minSpeed)/2 + minSpeed
+      
+      var time = calculateTime(midSpeed)
+      
+      if time <= hours {
+        answer = midSpeed
+        maxSpeed = midSpeed - 1
+      } else {
+        minSpeed = midSpeed + 1
+      }
+    }
+    return answer
+  }
+  
+  /*
+   Leetcode 1482: Minimum Number of Days to Make m Bouquets
+   */
+  func minDays(_ arr: [Int], _ m: Int, _ k: Int) -> Int {
+    var reqFlowers = m * k
+    var length = arr.count
+    
+    if length < reqFlowers {
+      return -1
+    }
+    
+    var lhs = arr.min()!
+    var rhs = arr.max()!
+    var minDays = rhs
+    
+    func ifPossible(_ day: Int) -> Bool {
+      var count = 0
+      var pointer = 0
+      
+      for i in 0 ..< length {
+        if arr[i] <= day {
+          pointer += 1
+        } else {
+          pointer = 0
+        }
+        
+        if pointer == k {
+          count += 1
+          pointer = 0
+        }
+      }
+      if count >= m {
+        return true
+      }
+      return false
+    }
+    
+    while lhs <= rhs {
+      var mid = (rhs - lhs)/2 + lhs
+      if ifPossible(mid) {
+        minDays = min(minDays, mid)
+        rhs = mid - 1
+      } else {
+        lhs = mid + 1
+      }
+    }
+    return minDays
+  }
+  
+  /*
+   Leetcode 1011: Capacity To Ship Packages Within D Days
+   */
+  func shipWithinDays(_ w: [Int], _ d: Int) -> Int {
+    var len = w.count
+    if len == 0 {
+      return 0
+    }
+    
+    var lhs = w.max()!
+    var rhs = w.reduce(0, +)
+    var reqW = rhs
+    
+    func ifPossible(_ weight: Int) -> Bool {
+      var count = 1
+      var pointer = weight
+      for i in 0 ..< len {
+        if pointer < w[i] {
+          pointer = weight
+          count += 1
+        }
+        pointer -= w[i]
+      }
+      if count <= d {
+        return true
+      }
+      return false
+    }
+    
+    while lhs <= rhs {
+      var mid = (rhs - lhs)/2 + lhs
+      
+      if ifPossible(mid) {
+        reqW = min(reqW, mid)
+        rhs = mid - 1
+      } else {
+        lhs = mid + 1
+      }
+    }
+    return reqW
+  }
+  
+  /*
+   
+   */
 }
